ecc902cec3192be4d5502902896f979c05747c89
dev-charan <raicharan100@gmail.com>
2025-07-19 14:50:18 +0530

    SignUp page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, Navigate } from 'react-router'
 import Home from './pages/Home'
 import Call from './pages/Call'
 import Login from './pages/Login'
@@ -11,29 +11,73 @@ import { Toaster } from 'react-hot-toast'
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance } from './lib/axios'
 
+const ProtectedRoute = ({ children, isAuth }) => {
+  return isAuth ? children : <Navigate to="/login" replace />
+}
+
 const App = () => {
-const {data, isLoading,error} = useQuery({queryKey:'todo',
-  queryFn:async()=>{
-    const res= axiosInstance.get("/auth/me")
-    
-    return res.data
-  }
-})
-console.log(data);
+  const { data: authData, isLoading, error } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      const res = await axiosInstance.get('/auth/me')
+      return res.data
+    },
+    retry: false,
+    refetchOnWindowFocus: false,
+  })
+
+  const currentUser = authData?.user
 
   return (
-    <div className='h-screen' data-theme="night">
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/notification' element={<Notification/>}/>
-        <Route path='/onboarding' element={<Onborad/>}/>
-        <Route path='/chat' element={<Chat/>}/>
-        <Route path='/call' element={<Call/>}/>
-    </Routes>
-    <Toaster/>
- </div>
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        {/* Public Routes */}
+       
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Routes */}
+         <Route path="/"  element={
+            <ProtectedRoute isAuth={!!currentUser}>
+              <Home/>
+            </ProtectedRoute>
+          }/>
+          
+        <Route
+          path="/notification"
+          element={
+            <ProtectedRoute isAuth={!!currentUser}>
+              <Notification />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/onboarding"
+          element={
+            <ProtectedRoute isAuth={!!currentUser}>
+              <Onborad />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute isAuth={!!currentUser}>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/call"
+          element={
+            <ProtectedRoute isAuth={!!currentUser}>
+              <Call />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Toaster />
+    </div>
   )
 }
 
