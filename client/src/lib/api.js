327a3682decc3ede1efc12d130ba28aa4e798d48
dev-charan <raicharan100@gmail.com>
2025-07-20 23:39:02 +0530

    feat: layout and logout functionality added

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -8,11 +8,19 @@ export const login = async(logindata)=>{
     const response = await axiosInstance.post("/auth/login",logindata)
     return response.data
 }
+export const logout = async()=>{
+    const response = await axiosInstance.post("/auth/logout")
+    return response.data
+}
 
 export const getAuthUser=async () => {
-      const res = await axiosInstance.get('/auth/me');
-      console.log(res.data);
-      return res.data;
+     try {
+         const res = await axiosInstance.get('/auth/me');
+         console.log(res.data);
+         return res.data;
+     } catch (error) {
+        return null
+     }
 }
 
 export const compleateOnloadring = async(userdata)=>{
